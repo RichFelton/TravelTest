1e95465f406ea660d2776307cc1c03d8bf88e723
RichFelton <rfelton.math@gmail.com>
2018-06-18 23:31:42 -0400

    first commit

---
+++ Travel.tests/TravelTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel.tests
+{
+    [TestFixture]
+    class TravelTests
+    {
+        [Test]
+        public void Test_Default_CTR()
+        {
+            Travels Japan = new Travels();
+        }
+
+    }
+}
